90aadd34fac12086b11626479896513eba4aa3f7
alwwwd <aslspbsochi@yandex.ru>
2022-05-14 14:13:08 +0300

    ready for check

--- домашка_1/крест.py
+++ домашка_1/крест.py
@@ -1,9 +1,9 @@
-for i in range(10 + 1):
-    for j in range(10 + 1):
+for i in range(10):
+    for j in range(10):
         if i == j:
-            print('^', end = ' ')
-        elif i == - j + 10:
-            print('^', end = ' ')
+            print('*', end = ' ')
+        elif i == - j + 9:
+            print('*', end = ' ')
         else:
             print(' ', end = ' ')
     print()
