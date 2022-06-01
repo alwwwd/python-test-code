a401f3098987f49d70562698d4fc60e44402dae1
alwwwd <aslspbsochi@yandex.ru>
2022-06-01 18:03:28 +0300

    all

--- домашка_5/app.py
+++ домашка_5/app.py
@@ -1,23 +1,31 @@
+def longest_words(file_name):
+    with open(file_name, encoding='utf-8') as file:
+        words = file.readline()
+        max_length = 0
 
- 
+        list_of_words = []
+        while words != "":
+            words = words.replace("\n", "")
+
+            if " " in words:
+                words = words.split(" ")
+            else:
+                words = words.split() 
+
+            for word in words:
+                current_word_length = len(word)
+                if current_word_length > max_length:
+                    list_of_words.clear()
+                    list_of_words.append(word)
+                    max_length = current_word_length
+                elif max_length == current_word_length:
+                    list_of_words.append(word)
+
+            words = file.readline()
+
+    return list_of_words
 
 
-
-f_t = open("/Users/alex/Desktop/test-python/домашка_5/article.txt", "r")
-
-
-
-
-
-def longest_words(file):
-    with open(file, encoding='utf-8') as text:
-        words = text.read().split()
-        max_length = len(max(words, key=len))
-        sought_words = [word for word in words if len(word) == max_length]
-        if len(sought_words) == 1:
-            return sought_words[0]
-        return sought_words
- 
  
 print(longest_words("/Users/alex/Desktop/test-python/домашка_5/article.txt"))
 
---
+++ домашка_6/task-5.py
@@ -0,0 +1,5 @@
+sp = " "
+st = "*"
+for i in range(0,4):
+    print(sp  * i, sp * i, st, sp  * i)
+    
