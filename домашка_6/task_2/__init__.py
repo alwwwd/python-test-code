ff593f066801b21d59d0b8deef9fc9275fad4960
alwwwd <aslspbsochi@yandex.ru>
2022-06-06 00:40:34 +0300

    error of task 2

---
+++ домашка_6/task_2/__init__.py
@@ -0,0 +1,27 @@
+from . import  rand_numbers, third , sort 
+
+
+
+rand_numbers_1 = []
+rand_numbers_2 = []
+rand_numbers_3 = []
+ 
+     
+
+
+
+
+
+print(rand_numbers(
+        rand_numbers_1,
+        rand_numbers_2))
+#print(max(rand_numbers_1))
+print(sum_lists(
+    rand_numbers_1,
+    rand_numbers_2,
+    rand_numbers_3))
+    
+
+print(sort(rand_numbers_3, "greater"))
+print(third())
+print(max(rand_numbers_3))
---
+++ домашка_6/task_2/third.py
@@ -0,0 +1,6 @@
+def third(
+    rand_numbers_3=rand_numbers_3,
+    range_max=10):
+    for i in range(1,10):
+        a = rand_numbers_3[i-1]+ rand_numbers_3[i]
+    return a
