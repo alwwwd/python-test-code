99eb6637cd3e59e0011fda788df842e5d4e95c30
alwwwd <aslspbsochi@yandex.ru>
2022-06-08 14:38:42 +0300

    task 3 in debug

--- домашка_1/sqare.py
+++ домашка_1/sqare.py
@@ -3,8 +3,7 @@ square = cycle - 1
 for i in range(cycle):
     for j in range(cycle):
         if i == 0 or i == square or j == 0 or j == square:
-            print("#", end = "%")
-
+            print("#", end = "")
         else:
             print(" ", end= "")
     print("")
--- домашка_6/task_3.py
+++ домашка_6/task_3.py
@@ -1,14 +1,13 @@
-a = input()
- 
-digits = "02468"
- 
-even = 0
-odd = 0
- 
-for i in a:
-    if i in digits:
-        even += 1
-    else:
-        odd += 1
- 
-print("Even: %d" % (even, odd))
+def number_is_digits(a):
+    digits = "02468" 
+    even = 0
+    odd = 0
+    for i in a:
+        if i in digits:
+            even += 1
+        else:
+            odd += 1
+    return even
+
+how_many_even_numbers = number_is_digits(input("введите числа:"))
+print(f"{how_many_even_numbers} чётных числа")
