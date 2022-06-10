11a2aad1895c863fbac1aeddf0d98d9030091e8a
alwwwd <aslspbsochi@yandex.ru>
2022-06-10 18:10:35 +0300

    task 1 ready for check

--- домашка_9/task_1.py
+++ домашка_9/task_1.py
@@ -1,7 +1,7 @@
 import random
 
 print(int(random.random() * 100) %  28 + 15)
-print(int(random.random() * 1000) % 699 + 306)
+print(int(random.random() * 10000) % 699 + 306)
 print(int(random.random() * 1000) % 7 + 97 )
 print(int(random.random() * 1000) % 243 + 86)
 
