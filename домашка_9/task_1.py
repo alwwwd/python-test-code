2fc2018ae084fe716e430beb355711a62bc033c0
alwwwd <aslspbsochi@yandex.ru>
2022-06-10 18:04:41 +0300

    task 1 ready for check

--- домашка_9/task_1.py
+++ домашка_9/task_1.py
@@ -1,9 +1,9 @@
 import random
 
 print(int(random.random() * 100) %  28 + 15)
-# print(int(random.random() * 1000) %  1004 + 307)
-# while True:
-#     print(int(random.random() * 1000) %  5 + 103)
-    
-# Сначала диапозон от 0 до 9. ПРи делении с остатком числа на 7 мы получаем диапозон от 0 до 6
-# После прибавления двойки к любому числу из диапозона от 0 до 6 мы получаем диапозон от 2 до 8
+print(int(random.random() * 1000) % 699 + 306)
+print(int(random.random() * 1000) % 7 + 97 )
+print(int(random.random() * 1000) % 243 + 86)
+
+
+
